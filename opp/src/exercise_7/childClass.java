45d2dcc9e347cab460e39115a0232e5fc5522967
sourabhgokhale <sourabhgokhale4@gmail.com>
2022-06-20 16:20:45 +0530

    rays

---
+++ opp/src/exercise_7/childClass.java
@@ -0,0 +1,20 @@
+package exercise_7;
+
+public class childClass extends parent1{
+	
+	
+	public childClass(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	}
+	
+	
+	
+	
+	
+	
+
+}
---
+++ opp/src/exercise_7/parent1.java
@@ -0,0 +1,42 @@
+package exercise_7;
+
+public class parent1 {
+	
+	public static  int num;
+	public String name ;
+	
+	
+	
+	public static int getNum() {
+		return num;
+	}
+
+
+
+
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+
+
+
+
+	public parent1(String name) {
+		super();
+		this.name = name;
+	}
+
+	
+	
+	
+	
+	
+	
+
+}
